ab1f2df3588e58856ed4bbf4ddec3aee45365675
Dhruvieeta <dhruvishah.eeta@gmail.com>
2025-08-23 00:57:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+// Add this logging middleware immediately after creating app
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
+
 app.use(express.json());
 
 const VERIFY_TOKEN = "mySecret123"; // Change this to your own token
